8a4691db5fba220e0dd1ce56f079eb00d0c31381
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-10-01 17:29:33 +0100

    added movies and showtimes

--- app.js
+++ app.js
@@ -9,17 +9,35 @@ $(function() {
 
 function listCinemas(cinemasArray){
   cinemasArray.forEach(function(cinema, index){
-    $("body").append("<p>" + cinema + "</p>")
+    $("body").append("<p><div class='cinema-name'>" + cinema.name + "</div><br>" + "<div class='movie-name'>" + listMovies(cinema.movies) + "<br></p>")
   }
   )
+}
+
+function listMovies(moviesArray){
+  var returnString = ""
+  moviesArray.forEach(function(movie, index) {
+    returnString +=  movie.name + "</div>"  + listShowtimes(movie.showtimes) + "<br>"
+  });
+  return returnString
+}
+
+function listShowtimes(showtimesArray) {
+  var showtimesString = ""
+  showtimesArray.forEach(function(showtime, movie) {
+    showtimesString += "<div class='showtime'>" + showtime + "</div>   "
+  });
+  return showtimesString
 }
 
 function cinemasApiSearch(searchTerm) {
   $.getJSON({
     url: 'http://localhost:3000/api/v1/cinemas/search?postcode='+ searchTerm,
   }) .done(function(res) {
+    console.log(res)
       listCinemas(res.cinemas);
   })
+
   .fail(function(err) {
     console.log('Error: ' + err.status);
   });
